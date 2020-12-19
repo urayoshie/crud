26857ee967b84b9fd3720088047c05b15f1bfcbd
Yoshie <yoshieura@icloud.com>
2020-12-19 15:23:58 +0900

    add redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+    redirect_to action: :index
   end
 
   def edit
@@ -19,11 +20,13 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
+    redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
+    redirect_to action: :index
   end
 
   private
